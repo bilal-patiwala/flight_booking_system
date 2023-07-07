3b0d8d52f2e012cf2c40312ca9336ab5b6fbba9f
bilal Patiwala <bilalpatiwala04@gmail.com>
2023-07-07 10:18:36 +0530

    vercel and build files added

--- flight_booking_system/settings.py
+++ flight_booking_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-fe819)mr2-0^3_9g!nlu98$wh@w%o0-otdaj%hy6hoi_0-g_te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','*']
 
 AUTH_USER_MODEL = 'flight_api.User'
 
